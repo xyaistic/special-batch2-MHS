1bfa0615742da18b4df22da633c85b127586b540
xyaistic <ziyaafridi880@gmail.com>
2024-11-19 13:34:19 +0530

    Initial commit

---
+++ src/Constant/api.js
@@ -0,0 +1,6 @@
+// export const  BaseUrl = `https://l3s24fdx-8000.inc1.devtunnels.ms`;
+export const  BaseUrl = `https://1zglhr6v-8000.inc1.devtunnels.ms`;
+export const createUser = `/api/register/`;
+export const loginuser = `/api/login/`;
+export const getAllproducts='/api/product/'
+export const addItemTocard='/api/item/'
---
+++ src/Pages/Layout.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import Header from './Header'
+import { Outlet,useNavigate } from 'react-router-dom'
+import Login from './Login'
+import Signup from './Signup';
+
+export default function Layout() {
+  const navigate = useNavigate();
+  const access = localStorage.getItem("access");
+  console.log(access,'token')
+  if(access!=null){
+    return (
+      <div>
+          <Header/>
+          <Outlet/>
+      </div>
+    )
+  }else{
+  // navigate("/login")
+  return(
+    <>
+    <div className=' flex  w-full'>
+    <Login/>
+    <Signup/>
+    </div>
+    
+    </>
+
+  )
+  }
+ 
+}
